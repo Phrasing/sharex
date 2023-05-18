e49c06eb3a315d742b32056b5553379fd83e8efe
Phrasing <mpbusiness94@gmail.com>
2023-05-18 18:40:29 -0400

    fixed potential path traversal, free() on state once done

--- main.cc
+++ main.cc
@@ -12,7 +12,6 @@
 // clang-format on
 
 namespace {
-constexpr const char *kHttpBindAddress = "http://0.0.0.0:80";
 constexpr const char *kHttpsBindAddress = "http://0.0.0.0:443";
 
 constexpr const char *kRootDir = "/home/weed/sharex-server";
@@ -20,6 +19,9 @@ constexpr const char *kUploadDir = "/home/weed/sharex-server/i";
 
 constexpr const char *kApiUsername = "bob@example.com";
 constexpr const char *kApiPassword = "example123";
+
+constexpr const char *kCertFile = "cert.pem";
+constexpr const char *kCertKeyFile = "privkey.pem";
 
 constexpr uint64_t kMaxFileSize = 1024 * 1024 * 1024;
 
@@ -32,8 +34,8 @@ enum http_status {
 };
 
 struct server_context {
-  bool use_tls = false;
   ::mg_http_serve_opts opts{};
+  ::mg_tls_opts tls_opts{};
 };
 
 struct mg_str_wrapper {
@@ -50,9 +52,12 @@ struct connection_state {
   std::future<http_status> future{};
 };
 
-void create_mg_context(server_context *ctx, bool use_tls = false) {
-  ctx->use_tls = use_tls;
+void create_mg_context(server_context *ctx) {
   ctx->opts = ::mg_http_serve_opts{.root_dir = kRootDir};
+  ctx->tls_opts = ::mg_tls_opts{
+      .cert = kCertFile,
+      .certkey = kCertKeyFile,
+  };
 }
 
 http_status handle_file_upload(std::unique_ptr<::mg_str_wrapper> body) {
@@ -65,7 +70,8 @@ http_status handle_file_upload(std::unique_ptr<::mg_str_wrapper> body) {
                       static_cast<int>(part.filename.len), part.filename.ptr);
     buf[length] = '\0';
 
-    std::string_view path_view = {buf, length};
+    const auto filtered_path = ::mg_remove_double_dots(buf);
+    std::string_view path_view = {filtered_path, ::strlen(filtered_path)};
 
     if (part.body.len > kMaxFileSize) {
       return http_status::HTTP_STATUS_BAD_REQUEST;
@@ -90,61 +96,50 @@ bool is_authorized_api_request(::mg_http_message *msg,
 void event_handler(::mg_connection *c, int ev, void *ev_data, void *fn_data) {
   auto *ctx = static_cast<server_context *>(fn_data);
 
-  if (ev == ::MG_EV_ACCEPT && ctx->use_tls) {
-    ::mg_tls_opts opts = {
-        .cert = "cert.pem",
-        .certkey = "privkey.pem",
-    };
-    ::mg_tls_init(c, &opts);
+  if (ev == ::MG_EV_ACCEPT && ctx != nullptr) {
+    ::mg_tls_init(c, &ctx->tls_opts);
   } else if (ev == ::MG_EV_HTTP_MSG) {
     auto *msg = static_cast<::mg_http_message *>(ev_data);
 
     if (::mg_http_match_uri(msg, "/api/upload") &&
         ::mg_vcasecmp(&msg->method, "POST") == 0) {
-      if (!is_authorized_api_request(msg, kApiUsername, kApiPassword)) {
-        ::mg_http_reply(c, http_status::HTTP_STATUS_UNAUTHORIZED, "",
-                        "Unauthorized");
-        return;
+      if (is_authorized_api_request(msg, kApiUsername, kApiPassword)) {
+        auto *state = reinterpret_cast<connection_state *>(
+            calloc(1, sizeof(connection_state)));
+
+        state->future = std::async(std::launch::async, &handle_file_upload,
+                                   std::move(std::make_unique<mg_str_wrapper>(
+                                       ::mg_strdup(msg->body))));
+
+        *reinterpret_cast<void **>(c->data) = state;
+      } else {
+        ::mg_http_reply(c, http_status::HTTP_STATUS_UNAUTHORIZED, "", "");
       }
-
-      auto *state = reinterpret_cast<connection_state *>(
-          calloc(1, sizeof(connection_state)));
-
-      state->future = std::async(
-          std::launch::async, &handle_file_upload,
-          std::move(std::make_unique<mg_str_wrapper>(::mg_strdup(msg->body))));
-
-      *reinterpret_cast<void **>(c->data) = state;
-
     } else {
       ::mg_http_serve_dir(c, static_cast<::mg_http_message *>(ev_data),
                           &ctx->opts);
     }
   } else if (ev == ::MG_EV_POLL) {
-    auto *state = *reinterpret_cast<connection_state**>(c->data);
+    auto *state = *reinterpret_cast<connection_state **>(c->data);
     if (state != nullptr && state->future.wait_for(std::chrono::seconds(0)) ==
-                               std::future_status::ready) {
+                                std::future_status::ready) {
+      auto *state_ptr = *reinterpret_cast<void **>(c->data);
       ::mg_http_reply(c, state->future.get(), "", "");
-      *reinterpret_cast<void **>(c->data) = nullptr;
+      free(state_ptr);
+      state_ptr = nullptr;
     }
   }
 }
 }  // namespace
 
-int main(void) {
+int32_t main(void) {
   mg_mgr mgr{};
   ::mg_log_set(MG_LL_INFO);
   ::mg_mgr_init(&mgr);
 
-  server_context http_context{};
-  server_context https_context{};
-
-  create_mg_context(&http_context);
-  create_mg_context(&https_context, true);
-
-  ::mg_http_listen(&mgr, kHttpBindAddress, event_handler, &http_context);
-
-  ::mg_http_listen(&mgr, kHttpsBindAddress, event_handler, &https_context);
+  server_context ctx{};
+  create_mg_context(&ctx);
+  ::mg_http_listen(&mgr, kHttpsBindAddress, event_handler, &ctx);
 
   for (;;) ::mg_mgr_poll(&mgr, 10);
   ::mg_mgr_free(&mgr);
